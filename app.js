b56620ec31bd686364678fb35f054499be8db9e2
Yurii <hapersy123@gmail.com>
2024-08-12 15:12:28 +0200

    first

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/main.html');
+   });
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
